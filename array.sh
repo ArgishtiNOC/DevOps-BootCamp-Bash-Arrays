4195499dffa9aec59f2c89cccfbe8bde7051682e
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2023-01-02 12:36:10 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -12,4 +12,9 @@ for i in {a..z}; do
  fi
 done
 
-echo "$x folders created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')";
+if [[ $x -gt 1 ]]
+then
+  echo "$num folders created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')";
+else
+  echo "$num folder created: $(ls -d folder_* | grep -v / | xargs echo | sed 's/ /, /g')"
+fi
