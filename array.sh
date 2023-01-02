f5ef861385520a021659b621c26f02669606c7bc
ArgishtiNOC <80466645+ArgishtiNOC@users.noreply.github.com>
2023-01-02 12:30:46 +0400

    Update array.sh

--- array.sh
+++ array.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 # Place your code here
+x=$1
+y=1
+
+for i in {a..z}; do
+  if [[ $y -le $x ]]
+  then
+    mkdir ./folder_$i
+    ((y++))
+ fi
+done
+
+echo "$x folders created: $(ls -d folder_* | grep -v / | tr '\n' ',')";
